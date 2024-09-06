9f5273de266565df6a0b2b151c8ccda70ba14ea6
Axuata <axuata@gmail.com>
2024-09-06 11:55:27 +0900

    いぇｙ

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,17 +4,17 @@ export default defineNuxtConfig({
   css: [
     '~/assets/styles/global.scss'
   ],
-  modules: [ '@nuxt/image'],
+  modules: [ '@nuxt/image' ],
   app:{
     head: {
-      title: "Your website name here",
+      title: "Axuata",
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
       ],
-      link: [
-        { rel: "icon", type: "image/png", href: "/favicon.png" }, // Favicon should be public/favicon.png
-      ],
+      // link: [
+      //   { rel: "icon", type: "image/png", href: "/favicon.png" }, // Favicon should be public/favicon.png
+      // ],
     },
   },
   components: [{
--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,32 @@
 </script>
 
 <template>
-  
+  <div class="axuata">
+    <h1>ようこそ</h1>
+    <p>私の名前は<b>Axuata</b>です（Yottakaと呼んでください）</p>
+    <p>茨城県に住んでいる学生で、現在は主に<b>ウェブ開発</b>に取り組んでいます。将来はウェブ開発者として働くことを目指しています。</p>
+    <p>趣味は<b>ゲーム</b>を楽しむことと、<b>プログラミング</b>です。時間があれば、新しい技術を学ぶことが好きです。</p>
+    <p>好きな言葉は「笑う門には福来る」です。</p>
+    <p><a href="https://x.com/axuata_x">X (Twitter)</a> / <a href="https://github.com/axuata">GitHub</a> / <a href="https://discord.gg/xz3Ar8fXN4">Discordサーバー</a> / <a href="https://misskey.io/@axuata">Misskey</a> / <a href="https://www.duolingo.com/profile/Axuata">Duolingo</a></p>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.axuata {
+  margin: 60px 120px;
+}
+
+h1 {
+  font-size: 50px;
+}
+
+h2 {
+  font-size: 30px;
+  margin-left: 5px;
+}
+
+p {
+  margin-top: 2px;
+  margin-left: 5px;
+}
+</style>
